1b016978f9b52a82c98e4d9d624413c11bce5421
David Nelson <49167208+dernestnelson@users.noreply.github.com>
2026-02-02 13:17:02 -0500

    Add initial unit test for SampleApp

---
+++ test/SampleApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace SampleApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void SampleTest_Passes()
+        {
+            Assert.True(true);
+        }
+    }
+}
